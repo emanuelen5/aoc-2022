9fed2c379dc54173c5c99e7d3dbe5d3079fdd741
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2022-12-02 20:44:21 +0000

    Creating helper scripts

---
+++ create_day.sh
@@ -0,0 +1,28 @@
+set -x
+
+_dir="day$@"
+if [ -f $_dir/__main__.py ]; then
+	echo "Directory '$_dir' already initialized" >2
+	exit 0
+fi
+
+mkdir -p $_dir
+touch $_dir/__init__.py $_dir/input.txt
+cat <<EOF > $_dir/__main__.py
+from pathlib import Path
+
+
+with open(Path(__file__).parent.joinpath("input.txt")) as f:
+	data = f.read()
+
+part1 = None
+part2 = None
+
+print(f"Part 1: {part1}")
+print(f"Part 2: {part2}")
+
+EOF
+
+git add $_dir
+git commit -m "Creating empty $_dir"
+
