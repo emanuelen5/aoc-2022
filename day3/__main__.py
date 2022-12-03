30cec28e4de3e1ec4b770f523e987d8020f6646e
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2022-12-03 13:38:35 +0000

    Solve day 3 part 2

--- day3/__main__.py
+++ day3/__main__.py
@@ -3,11 +3,14 @@ from . import lib
 
 
 with open(Path(__file__).parent.joinpath("input.txt")) as f:
-	compartments = [lib.line_to_compartments(line) for line in f.readlines()]
+    lines = [line.strip() for line in f.readlines()]
+
+compartments = [lib.line_to_compartments(line) for line in lines]
+groups = lib.group_lines(lines)
 
 
 part1 = sum(lib.compartments_to_score(c1, c2) for c1, c2 in compartments)
-part2 = None
+part2 = sum(lib.group_to_score(group) for group in groups)
 
 print(f"Part 1: {part1}")
 print(f"Part 2: {part2}")
--- day3/lib.py
+++ day3/lib.py
@@ -1,5 +1,5 @@
 from functools import lru_cache
-from typing import Tuple
+from typing import Tuple, List, Set
 
 
 @lru_cache()
@@ -8,6 +8,7 @@ def item_to_score(item: str):
         return ord(item) - ord('a') + 1
     if ord('A') <= ord(item) <= ord('Z'):
         return ord(item) - ord('A') + 27
+    raise ValueError(f"Unhandled value {item}")
 
 
 def line_to_compartments(line) -> Tuple[str, str]:
@@ -18,7 +19,21 @@ def line_to_compartments(line) -> Tuple[str, str]:
     return c1, c2
 
 
+def common_item_to_score(item_set: Set[str]) -> int:
+    assert len(item_set) == 1, "Only one item should be common to be able to calculate the score"
+    return item_to_score(next(i for i in item_set))
+
+
 def compartments_to_score(c1, c2) -> int:
     duplicate_items = set(c1).intersection(set(c2))
-    assert len(duplicate_items) == 1, "Only one item should be common between the compartments"
-    return item_to_score(next(i for i in duplicate_items))
+    return common_item_to_score(duplicate_items)
+
+
+group_t = Tuple[str, str, str]
+
+def group_lines(lines) -> List[group_t]:
+    return zip(lines[::3], lines[1::3], lines[2::3])
+
+def group_to_score(group: group_t) -> int:
+    duplicate_items = set(group[0]).intersection(group[1]).intersection(group[2])
+    return common_item_to_score(duplicate_items)
