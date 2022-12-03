fe64c9b7ad9100e2cd7c3ecc661bffd2605354eb
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2022-12-03 13:39:58 +0000

    Adding test input

--- create_day.sh
+++ create_day.sh
@@ -24,8 +24,13 @@ print(f"Part 2: {part2}")
 EOF
 
 cat <<EOF > $_dir/test.py
+from pathlib import Path
 import unittest
 import lib
+
+
+with open(Path(__file__).parent.joinpath("test_input.txt"), 'r') as f:
+    test_input = [line.strip for line in f.readlines()]
 
 
 tc = unittest.TestCase()
