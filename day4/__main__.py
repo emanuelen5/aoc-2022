df22316a18d279c9a8d14607a97c965d769ad03e
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2022-12-04 11:00:21 +0000

    Solve day 4 part 1

--- day4/__main__.py
+++ day4/__main__.py
@@ -3,9 +3,11 @@ from . import lib
 
 
 with open(Path(__file__).parent.joinpath("data/input.txt"), 'r', encoding="utf-8") as f:
-    data = f.read()
+    assignments = [lib.line_to_ranges(line) for line in f.readlines()]
 
-part1 = None
+fully_contained_ranges = sum(1 for a1, a2 in assignments if lib.is_either_fully_contained_within_other(a1, a2))
+
+part1 = fully_contained_ranges
 part2 = None
 
 print(f"Part 1: {part1}")
--- day4/test.py
+++ day4/test.py
@@ -4,7 +4,19 @@ import lib
 
 
 with open(Path(__file__).parent.joinpath("data/test_input.txt"), 'r', encoding="utf-8") as f:
-    test_input = [line.strip for line in f.readlines()]
+    test_input = [line.strip() for line in f.readlines()]
 
 
 tc = unittest.TestCase()
+
+tc.assertEqual(slice(2, 4), lib.range_str_to_slice("2-4"), "One digit each")
+tc.assertEqual(slice(11, 50), lib.range_str_to_slice("11-50"), "Two digits")
+
+tc.assertEqual((slice(2, 4), slice(6, 8)), lib.line_to_ranges(test_input[0]))
+
+tc.assertFalse(lib.is_either_fully_contained_within_other(*lib.line_to_ranges(test_input[0])))
+tc.assertFalse(lib.is_either_fully_contained_within_other(*lib.line_to_ranges(test_input[1])))
+tc.assertFalse(lib.is_either_fully_contained_within_other(*lib.line_to_ranges(test_input[2])))
+tc.assertTrue(lib.is_either_fully_contained_within_other(*lib.line_to_ranges(test_input[3])))
+tc.assertTrue(lib.is_either_fully_contained_within_other(*lib.line_to_ranges(test_input[4])))
+tc.assertFalse(lib.is_either_fully_contained_within_other(*lib.line_to_ranges(test_input[5])))
