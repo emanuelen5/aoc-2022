0676b5e035fd7257b26d832b2fa7dfae8b51dc54
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2022-12-04 11:08:33 +0000

    Correct lapsus in template

--- template/test.py
+++ template/test.py
@@ -4,7 +4,7 @@ import lib
 
 
 with open(Path(__file__).parent.joinpath("data/test_input.txt"), 'r', encoding="utf-8") as f:
-    test_input = [line.strip for line in f.readlines()]
+    test_input = [line.strip() for line in f.readlines()]
 
 
 tc = unittest.TestCase()
