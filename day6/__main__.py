90bb2f45089dbaa4b5682c854f0d0571de6e7d11
Erasmus Cedernaes <erasmus.cedernaes@gmail.com>
2022-12-06 20:29:44 +0000

    Solve day 6 part 2

--- day6/__main__.py
+++ day6/__main__.py
@@ -6,7 +6,7 @@ with open(Path(__file__).parent.joinpath("data/input.txt"), 'r', encoding="utf-8
     lines = f.read().split("\n")
 
 part1 = lib.get_start_of_packet_end(lines[0])
-part2 = None
+part2 = lib.get_start_of_packet_end(lines[0], 14)
 
 print(f"Part 1: {part1}")
 print(f"Part 2: {part2}")
--- day6/lib.py
+++ day6/lib.py
@@ -1,6 +1,6 @@
-def get_start_of_packet_end(line: str) -> int:
-    for i in range(0, len(line) - 4):
-        unique_letters = set(line[i:i+4])
-        if len(unique_letters) == 4:
+def get_start_of_packet_end(line: str, unique_count=4) -> int:
+    for i in range(0, len(line) - unique_count):
+        unique_letters = set(line[i:i+unique_count])
+        if len(unique_letters) == unique_count:
             break
-    return i + 4
+    return i + unique_count
